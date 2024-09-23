dc9632f45764be2491651ce8ad7cabcfc6f946f7
James2898 <amajamesmartinez@gmail.com>
2024-09-24 02:15:55 +0800

    API Get Students List

--- main.go
+++ main.go
@@ -16,8 +16,17 @@ import (
 )
 
 type Student struct {
-	ID   primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name string             `json:"name"`
+	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	LastName   string             `json:"lastName"`
+	FirstName  string             `json:"firstName"`
+	MiddleName string             `json:"middleName"`
+	Suffix     string             `json:"suffix"`
+	Contact    string             `json:"contact"`
+	ImageUri   string             `json:"imageUri"`
+	SectionID  int                `json:"sectionID"`
+	Strand     string             `json:"strand"`
+	Grade      int                `json:"grade"`
+	Comment    string             `json:"comment"`
 }
 
 var studentsCol *mongo.Collection
